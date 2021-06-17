adb5dd255d1e7afcff3940e845df87abeccab4c5
hi-m0narch <83179992+hi-m0narch@users.noreply.github.com>
2021-06-17 15:12:09 +0700

    contoh simpleapikey

---
+++ rest-api.php
@@ -0,0 +1,38 @@
+<?php 
+
+    function getKey(){
+        return["1234", "rahasia", "abcd"];
+    }
+
+    function isValid($input) {
+        $apikey = $input["api_key"];
+        if (in_array($apikey, getKey())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function jsonOut($status, $message, $data) {
+        $respon = ["status" => $status, "message" => $message, "data" => $data];
+        
+        header("Content-type: application/json");
+        echo json_encode($respon);
+    }
+
+    function getFilm(){
+        $film = [
+        ["title" => "FF9", "konten" => "film ini film ke-9"],
+        ["title" => "FF1", "konten" => "film ini film ke-1"],
+        ["title" => "FF2", "konten" => "film ini film ke-2"]
+    ];
+    return $film;    
+}
+
+if (isValid($_GET)){
+    jsonOut("berhasil", "apikey valid"), getFilm());
+} else {
+    jsonOut("gagal", "apikey not valid", null);
+}
+
+?>

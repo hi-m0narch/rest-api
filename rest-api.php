adcb81ca483ba02c4041d18bcfc65e6a22ee7de3
hi-m0narch <83179992+hi-m0narch@users.noreply.github.com>
2021-06-24 08:05:46 +0700

    Update rest-api.php

--- rest-api.php
+++ rest-api.php
@@ -1,38 +1,42 @@
-<?php 
+<?php
 
-    function getKey(){
-        return["1234", "rahasia", "abcd"];
-    }
-
-    function isValid($input) {
-        $apikey = $input["api_key"];
-        if (in_array($apikey, getKey())) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    function jsonOut($status, $message, $data) {
-        $respon = ["status" => $status, "message" => $message, "data" => $data];
-        
-        header("Content-type: application/json");
-        echo json_encode($respon);
-    }
-
-    function getFilm(){
-        $film = [
-        ["title" => "FF9", "konten" => "film ini film ke-9"],
-        ["title" => "FF1", "konten" => "film ini film ke-1"],
-        ["title" => "FF2", "konten" => "film ini film ke-2"]
-    ];
-    return $film;    
+function getKey()
+{
+    return ["1234", "rahasia", "xyz"];
 }
 
-if (isValid($_GET)){
-    jsonOut("berhasil", "apikey valid"), getFilm());
+function isValid($input)
+{
+    $apikey = $input["api_key"];
+    if (in_array($apikey, getKey())) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function jsonOut($status, $message, $data)
+{
+    $respon = ["status" => $status, "message" => $message, "data" => $data];
+
+    header("Content-type: application/json");
+    echo json_encode($respon);
+}
+
+function getFilm()
+{
+    $film = [
+        ["title" => "FF1", "konten" => "film ini ke-1"],
+        ["title" => "FF2", "konten" => "film ini ke-2"],
+        ["title" => "FF9", "konten" => "film ini ke-9"]
+    ];
+    return $film;
+}
+
+if (isValid($_GET)) {
+    jsonOut("berhasil", "apikey valid", getFilm());
 } else {
-    jsonOut("gagal", "apikey not valid", null);
+    jsonOut("gagal", "apikey no valid!!", null);
 }
 
 ?>
